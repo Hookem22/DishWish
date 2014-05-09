f3924a5cf75857a0896c0548a09d07544168d138
Hookem22 <williamallenparks@gmail.com>
2014-05-09 16:37:55 -0500

    Initial Commit

---
+++ DishWish/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DishWish
+//
+//  Created by Will on 5/9/14.
+//  Copyright (c) 2014 DishWish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DishWishTests/DishWishTests.m
@@ -0,0 +1,34 @@
+//
+//  DishWishTests.m
+//  DishWishTests
+//
+//  Created by Will on 5/9/14.
+//  Copyright (c) 2014 DishWish. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DishWishTests : XCTestCase
+
+@end
+
+@implementation DishWishTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
